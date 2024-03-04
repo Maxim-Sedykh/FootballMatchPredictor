8118d8ac3c7f589dd2741602e0dcedd0856f4ea9
Maxim Sedykh <max_se@bk.ru>
2024-03-04 11:42:46 +0300

    Добавьте файлы проекта.

---
+++ FootballMatchPredictor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballMatchPredictor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
